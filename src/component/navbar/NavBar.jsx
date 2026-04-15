1bb3444cf04f3f0509c6b8a940738a9cc8a38866
saiful islam <mdsaifulofficial2@gmail.com>
2026-04-15 16:04:07 -0700

    newupdate

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/image/logo_no_bg_v2.png'
+import { Link } from "react-router-dom";
+import logo from "../../assets/image/logo_no_bg_v2.png";
 
 function NavBar() {
   return (
@@ -10,7 +10,7 @@ function NavBar() {
         {/* LEFT */}
         <div className="navbar-start">
 
-          {/* Mobile Menu */}
+          {/* MOBILE MENU */}
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -19,19 +19,19 @@ function NavBar() {
             </div>
 
             <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-xl mt-3 w-56 p-3 shadow-lg z-[1]">
-              <li><a>Home</a></li>
-              <li><a>Investor License</a></li>
-              <li><a>Visa Service</a></li>
-              <li><a>General Service</a></li>
-              <li><a>About Us</a></li>
-              <li><a>Contact</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/investorlicense">Investor License</Link></li>
+              <li><Link to="/visaServices">Visa Service</Link></li>
+              <li><Link to="/general-services">General Service</Link></li>
+              <li><Link to="/about">About Us</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
             </ul>
           </div>
 
-          {/* Logo */}
-          <a className="flex items-center gap-2">
-            <img className="h-18 w-auto" src={logo} alt="Logo" />
-          </a>
+          {/* LOGO */}
+          <Link to="/" className="flex items-center gap-2">
+            <img className="h-16 w-auto" src={logo} alt="Logo" />
+          </Link>
 
         </div>
 
@@ -45,10 +45,12 @@ function NavBar() {
               <details>
                 <summary>Services</summary>
                 <ul className="p-2 bg-base-100 w-48 rounded-xl shadow-md">
-                  <li><a>Visa Services</a></li>
-                  <li><a>General Services</a></li>
-                  <li><a>Business Services</a></li>
-                  <li><a>Investor Services</a></li>
+ <li><Link to="/investorlicense">Investor Services</Link></li>
+                  <li><Link to="/visaservice">Visa Services</Link></li>
+                  <li><Link to="/general">General Services</Link></li>
+                  <li><Link to="/business">Business Services</Link></li>
+                 
+
                 </ul>
               </details>
             </li>
@@ -56,31 +58,33 @@ function NavBar() {
             <li><Link to="/investorlicense">Investor License</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/addtocard">
-            ADD TO CARD
-            </Link></li>
+            <li><Link to="/addtocard">ADD TO CART</Link></li>
 
           </ul>
         </div>
 
         {/* RIGHT */}
-        <div className="navbar-end gap-3 bg-gradient-to-r from-[#16166b] to-[#1105f3] bg-clip-text text-transparent">
+        <div className="navbar-end gap-3">
 
-          <a className="  btn btn-ghost hidden sm:inline-flex">
+          <Link to="/login" className="btn btn-ghost hidden sm:inline-flex">
             Login
-          </a>
-<Link to="/contact" className="px-6 py-3 rounded-md text-white font-semibold 
-bg-gradient-to-r from-[#16166b] to-[#1105f3] 
-hover:from-[#1105f3] hover:to-[#16166b] 
-transition-all duration-300">
-  Contact US
-</Link>
+          </Link>
+
+          <Link
+            to="/contact"
+            className="px-6 py-3 rounded-md text-white font-semibold 
+            bg-gradient-to-r from-[#16166b] to-[#1105f3] 
+            hover:from-[#1105f3] hover:to-[#16166b] 
+            transition-all duration-300"
+          >
+            Contact US
+          </Link>
 
         </div>
 
       </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
